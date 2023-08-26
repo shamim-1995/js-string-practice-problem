c4eb96da4fddc8422fbb1d5860b315403b7d8d6e
shamim-1995 <sashamim987@gmail.com>
2023-08-26 12:46:44 +0600

    added max number section

---
+++ max.js
@@ -0,0 +1,14 @@
+const jim = 84;
+const dela = 99;
+const chinku = 77;
+if(jim > dela && jim > chinku){
+    console.log('jim will get the cake');
+}
+else if(dela > jim && dela > chinku){
+    console.log('Dela you will get the cake');
+}
+else{
+    console.log('chinku you will get the cake');
+}
+
+
